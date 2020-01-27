3e8e1bba34db62210fa7c717c6d7534f043b5806
Gaurav Kumar <gauravk26800@gmail.com>
2020-01-27 21:36:50 +0530

    initial commit testing openCV

---
+++ test_OpenCV.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+while(1):                       #video loop
+    ret, frame = cap.read()     #read the image
+    cv2.imshow('image', frame)  #show the image
+    if cv2.waitKey(1) == 27:     #27 is the ascii value for escape key, ends the video loop
+        break
+
+cv2.destroyAllWindow()
+cv2.release()
